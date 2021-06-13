b20925a68c6d3ad5b330dcff5733cc7b2a03ae75
ADI <64772793+adi4fab@users.noreply.github.com>
2021-06-13 19:47:11 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -30,7 +30,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraformansibledeplymentintegration"
+    bucket = "terraformtfstatelocation"
     key    = "terraform.tfstate"
     region = "us-east-1"
    # profile = "terraform_user"
